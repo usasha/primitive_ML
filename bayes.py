2a7b69156bf86d445d75fabced8cc1a836349967
uSasha <sasha.engineer@gmail.com>
2018-01-01 18:11:18 +0300

    common functionality moved to abstract class

--- bayes.py
+++ bayes.py
@@ -1,7 +1,8 @@
 import numpy as np
+import abc
 
 
-class NaiveBayes(object):
+class NaiveBayesBase(abc.ABC):
     def __init__(self):
         self.probabilities = []
         self.x = None
@@ -11,43 +12,39 @@ class NaiveBayes(object):
         self.x = np.array(x)
         self.y = np.array(y)
 
-    def _get_proba_multinom(self, feature, point, category):
+    @abc.abstractmethod
+    def _get_proba(self, feature, point, category):
+        pass
+
+    def predict(self, x):
+        result = []
+        for point in x:
+            probabilities = []
+            for category in np.unique(self.y):
+                category_prob = 1
+                for feature in range(len(point)):
+                    category_prob *= self._get_proba(feature, point, category)
+
+                probabilities.append(category_prob)
+            result.append(np.unique(self.y)[np.argmax(probabilities)])
+        return result
+
+
+class NaiveBayesMultinominal(NaiveBayesBase):
+    def _get_proba(self, feature, point, category):
         p_a = (self.y == category).sum() / len(self.y)
         p_ba = ((self.x[self.y == category][:, feature] == point[feature]).sum()
                 / len(self.x[self.y == category]))
         p_b = (self.x[:, feature] == point[feature]).sum() / len(self.x) + 1e-100
+
         return p_a * p_ba / p_b
 
-    def predict(self, x):
-        result = []
-        for point in x:
-            probabilities = []
-            for category in np.unique(self.y):
-                category_prob = 1
-                for feature in range(len(point)):
-                    category_prob *= self._get_proba_multinom(feature, point, category)
 
-                probabilities.append(category_prob)
-            result.append(np.unique(self.y)[np.argmax(probabilities)])
-        return result
-
-class NaiveBayesGaussian(NaiveBayes):
-    def _get_proba_gaussian(self, feature, point, category):
+class NaiveBayesGaussian(NaiveBayesBase):
+    def _get_proba(self, feature, point, category):
         std = np.std(self.x[self.y == category])
         u = np.mean(self.x[self.y == category])
         prob = (1 / np.sqrt(2 * np.pi * std ** 2)
                 * np.exp(-(point[feature] - u) ** 2 / 2 * std ** 2))
+
         return prob
-
-    def predict(self, x):
-        result = []
-        for point in x:
-            probabilities = []
-            for category in np.unique(self.y):
-                category_prob = 1
-                for feature in range(len(point)):
-                    category_prob *= self._get_proba_gaussian(feature, point, category)
-
-                probabilities.append(category_prob)
-            result.append(np.unique(self.y)[np.argmax(probabilities)])
-        return result
