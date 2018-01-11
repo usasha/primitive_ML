af1ba7b594f4fbf640b07ce0c5e7cae9407aea84
uSasha <sasha.engineer@gmail.com>
2018-01-11 22:48:50 +0300

    gaussian bayes tests

--- tests/test_bayes.py
+++ tests/test_bayes.py
@@ -1,4 +1,6 @@
 import pytest
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
 import numpy as np
 import sys
 from sklearn import datasets
@@ -29,11 +31,73 @@ def test_multinominal_fit():
 
 def test_multinominal_get_proba():
     dataset = datasets.load_digits()
+    x = dataset.data
+    y = dataset.target
+
+    clf = NaiveBayesMultinominal()
+    clf.fit(x, y)
+    class_0 = clf._get_proba(2, x[0], 0)
+    class_1 = clf._get_proba(2, x[0], 1)
+    class_2 = clf._get_proba(2, x[0], 2)
+    assert class_0 > class_1 > class_2
+
+
+def test_multinominal_predict():
+    dataset = datasets.load_digits()
+    x = dataset.data
+    y = dataset.target
+
+    clf = NaiveBayesMultinominal()
+    clf.fit(x, y)
+    y_pred = clf.predict(x)
+    precision = precision_score(y, y_pred, average='micro')
+    recall = recall_score(y, y_pred, average='micro')
+
+    assert precision > 0.8
+    assert recall > 0.8
+
+
+def test_gauss_init():
+    clf = NaiveBayesGaussian()
+    assert clf.x is None
+    assert clf.y is None
+    assert clf.probabilities == []
+
+
+def test_gauss_fit():
+    dataset = datasets.load_digits()
     data = dataset.data
     target = dataset.target
 
-    clf = NaiveBayesMultinominal()
+    clf = NaiveBayesGaussian()
     clf.fit(data, target)
-    assert clf._get_proba(2, data[0], 0) == pytest.approx(0.01168, 0.001)
-    assert clf._get_proba(2, data[0], 1) == pytest.approx(0.00612, 0.001)
-    assert clf._get_proba(2, data[0], 2) == pytest.approx(0.003895, 0.001)
+    assert np.array_equal(clf.x, data)
+    assert np.array_equal(clf.y, target)
+
+
+def test_gauss_get_proba():
+    dataset = datasets.load_iris()
+    x = dataset.data
+    y = dataset.target
+
+    clf = NaiveBayesGaussian()
+    clf.fit(x, y)
+    class_0 = clf._get_proba(0, x[0], 0)
+    class_1 = clf._get_proba(0, x[0], 1)
+    class_2 = clf._get_proba(0, x[0], 2)
+    assert class_0 < class_1 < class_2
+
+
+def test_gauss_predict():
+    dataset = datasets.load_iris()
+    x = dataset.data
+    y = dataset.target
+
+    clf = NaiveBayesGaussian()
+    clf.fit(x, y)
+    y_pred = clf.predict(x)
+    precision = precision_score(y, y_pred, average='micro')
+    recall = recall_score(y, y_pred, average='micro')
+
+    assert precision > 0.7
+    assert recall > 0.7
